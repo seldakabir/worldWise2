573724934106ced5f2a8a6e09d142fc04302b574
selda kabir <seldaakabirnejat@gmail.com>
2024-07-25 20:30:05 +0300

    addPosition

--- src/Components/CityItem.jsx
+++ src/Components/CityItem.jsx
@@ -1,7 +1,7 @@
 import styles from "./CityItem.module.css";
 import { Link } from "react-router-dom";
 export default function CityItem({ city }) {
-  const { emoji, cityName, date, id } = city;
+  const { emoji, cityName, date, id, position } = city;
   const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
       day: "numeric",
@@ -11,7 +11,10 @@ export default function CityItem({ city }) {
     }).format(new Date(date));
   return (
     <li>
-      <Link to={`${id}`} className={styles.cityItem}>
+      <Link
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+        className={styles.cityItem}
+      >
         <span className={styles.imoji}>{emoji} </span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
