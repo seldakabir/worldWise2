84b3ccbd0fb0b7bf944e0fef414ba9cf1d65cd47
selda kabir <seldaakabirnejat@gmail.com>
2024-07-25 18:40:30 +0300

    cityList

--- src/Components/CityItem.jsx
+++ src/Components/CityItem.jsx
@@ -1,11 +1,19 @@
-import styles from './CityItem.module.css'
-export default function CityItem({city}) {
+import styles from "./CityItem.module.css";
+export default function CityItem({ city }) {
+  const { imoji, cityName, date } = city;
+  const formatDate = (date) =>
+    new Intl.DateTimeFormat("en", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+      weekday: "long",
+    }).format(new Date(date));
   return (
-      <li className={styles.cityItem}>
-          {city}
-
-
+    <li className={styles.cityItem}>
+      <span className={styles.imoji}>{imoji} </span>
+      <h3 className={styles.name}>{cityName}</h3>
+      <time className={styles.date}>{formatDate(date)}</time>
+      <button className={styles.deleteBtn}>&times;</button>
     </li>
-  )
+  );
 }
-
--- src/Components/CityList.jsx
+++ src/Components/CityList.jsx
@@ -1,25 +1,17 @@
- import styles from './CityList.module.css';
- import CityItem from './CityItem';
- import Spinner from './Spinner.jsx';
+import styles from "./CityList.module.css";
+import CityItem from "./CityItem";
+import Spinner from "./Spinner.jsx";
+import Message from "./Message.jsx";
 
-export default function CityList({ cities,isLoading}) {
-
- 
-
+export default function CityList({ cities, isLoading }) {
+  if (!cities.length) return <Message message="Add your first city" />;
   if (isLoading) return <Spinner />;
 
-return (
-  <ul className={styles.cityList}>
-    {
-      cities
-    }
-
-    {/* {cities.map(city => (
-      <CityItem key={city.id} city={city} />
-    ))} */}
-  </ul>
-);
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem key={city.id} city={city} />
+      ))}
+    </ul>
+  );
 }
-
-
-
