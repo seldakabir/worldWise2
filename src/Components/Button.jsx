f239375c0a17f4f12dc0061c1e2e080964956973
selda kabir <seldaakabirnejat@gmail.com>
2024-07-26 12:12:39 +0300

    buttonClass

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,7 @@
 import styles from "./Button.module.css";
 export default function Button({ children, onClick, type }) {
   return (
-    <button onClick={onClick} className={`${styles.btn}${styles[type]}`}>
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
       {children}
     </button>
   );
